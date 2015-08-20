37b83319f1ce3ab62fc5d944cb641f0eeca027a1
Ben Duncan <duncan.ben@gmail.com>
2015-08-19 21:52:01 -0700

    adding font files for roboto

---
+++ src/controllers/notifications.cordova.js
@@ -0,0 +1,58 @@
+import moment from 'moment';
+import Singletons from 'app/controllers/singletons';
+import Actions from 'app/actions';
+import ACTIONS from 'app/constants/actionTypes';
+
+let apiGetter = (function() {
+  let resolve, reject;
+  let promise = new Promise(function(res, rej) {
+    resolve = res;
+    reject = rej;
+  });
+  return { resolve, reject, promise };
+}());
+
+function setWithAPI(options) {
+  this.schedule({
+    id: options.id,
+    title: options.title,
+    text: options.text,
+    led: '005A4E',
+    at: moment(options.at).toDate()
+  });
+}
+function removeWithAPI(id) {
+  this.cancel(id);
+}
+
+var Controller = {
+
+  remove(id) {
+    apiGetter.promise.then((api) => {
+      removeWithAPI.call(api, id);
+    });
+  },
+
+  set(options) {
+    apiGetter.promise.then((api) => {
+      setWithAPI.call(api, options);
+    });
+  },
+
+  onReady() {
+    apiGetter.resolve(cordova.plugins.notification.local);
+    // Reset fired notifications for tomorrow
+    cordova.plugins.notification.local.on('trigger', function(notification) {
+      console.warn('alerting', notification.id);
+      Singletons.Store.update(
+        undefined,
+        Actions[ACTIONS.TRIGGER_ALERT](notification.id)
+      );
+    });
+  }
+
+};
+
+Singletons.NotificationsController = Controller;
+
+export default Controller;
---
+++ src/controllers/singletons.js
@@ -0,0 +1,2 @@
+// Attach singletons here, and this can be called to avoid circular dependencies
+export default {};
